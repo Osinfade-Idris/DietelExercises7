2153be4504c885fe7958c3b527a6659c59edc1a6
Osinfade Idris <osinfadeidris@gmail.com>
2022-07-18 18:08:17 +0100

    commit

--- src/PlayingCard/DeckOfCards.java
+++ src/PlayingCard/DeckOfCards.java
@@ -11,7 +11,7 @@ public class DeckOfCards {
 
     public DeckOfCards(){
         String[] faces = {"Ace", "Deuce", "Three", "Four",
-        "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+        "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
         String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
 
         deck=new Card[NUMBER_OF_CARDS];
@@ -27,7 +27,16 @@ public class DeckOfCards {
         for (int first = 0; first < deck.length; first++) {
             int second = randomNumbers.nextInt(NUMBER_OF_CARDS);
 
-
+            Card temp = deck[first];
+            deck[first]=deck[second];
+            deck[second]=temp;
         }
+    }
+
+    public  Card dealCard(){
+        if(currentCard< deck.length)
+            return deck[currentCard++];
+        else
+            return null;
     }
 }
